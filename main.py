30ab9be4dc075268e9a8aea1c7f573f9f6210844
Mara Andronache <mararux05@gmail.com>
2023-07-19 11:39:52 +0300

    yask 3

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ city = input("What city are you from? ")
 gender = input("What is your gender? ")
 education = input("What type of education did you finish last? (middle school, high school, college, masters, PhD, etc.) ")
 pets = input("Do you own pets? ")
+
+print(f"The respondent is {age} years old, {gender}, from {city} city. The higher level of education they have finished is {education}.")
