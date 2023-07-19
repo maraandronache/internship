b7858c22a60f3923becb9202011677349f078652
Mara Andronache <mararux05@gmail.com>
2023-07-19 12:39:14 +0300

    task 4

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-age = input("What is your age? ")
-city = input("What city are you from? ")
+while True:
+    age = input("What is your age? ")
+    # using the isnumeric function to check if every character is a number in order to validate age input
+    if age.isnumeric():
+        break
+
+while True:
+    city = input("What city are you from? ")
+    # using the isalpha function to check if every character is in the alphabet in order to validate city
+    if city.isalpha():
+        break
+
 gender = input("What is your gender? ")
 education = input("What type of education did you finish last? (middle school, high school, college, masters, PhD, etc.) ")
-pets = input("Do you own pets? ")
+
+while True:
+    pets = input("Do you own pets? ")
+    if pets == 'yes' or pets == 'y' or pets == 'Yes' or pets == 'no' or pets == 'n' or pets == 'No':
+        break
 
 print(f"The respondent is {age} years old, {gender}, from {city} city. The higher level of education they have finished is {education}.")
+
