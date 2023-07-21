d43eef8c5ff08f0f81c4c88e33a9c5ee7c5f0586
Mara Andronache <mararux05@gmail.com>
2023-07-21 14:31:39 +0300

    updated with around 1/2 of the tasks for the report file

--- interpret_answers.py
+++ interpret_answers.py
@@ -9,6 +9,8 @@ def main():
     pet_names = []
     male_cnt = 0
     female_cnt = 0
+    male_owners = 0
+    female_owners = 0
     with open("input.csv", 'r') as file:
         reader = csv.reader(file)
         for row in reader:
@@ -17,6 +19,10 @@ def main():
             if row[4] == 'True':
                 owners_ages.append(int(row[0]))
                 num = int(row[5])
+                if row[2] == 'male':
+                    male_owners += 1
+                else:
+                    female_owners += 1
                 for i in range(num):
                     pet_names.append(row[num + i + 1]) # adding all names of pets in pet_names
                     pet_types.append(row[num + i + 2])
@@ -33,11 +39,28 @@ def main():
     total_resp = len(all_ages)
     owns_pets = len(owners_ages)
     not_owns_pets = len(non_owners_ages)
-    percentage_owners = round(percentage(owns_pets, not_owns_pets), 2)
+    percentage_owners = percentage(owns_pets, not_owns_pets)
+    percentage_gender = percentage(male_cnt, female_cnt)
+    percentage_gender_owners = percentage(male_owners, female_owners)
 
     report.write(f"A total number of {total_resp} people have taken this survey, out of which {owns_pets} own pets and "
-                 f"{not_owns_pets} do not.\n")
-    report.write(f"Overall, about {percentage_owners}% own pets, and {100-percentage_owners}% do not.")
+                 f"{not_owns_pets} do not.\nThe average age of all the respondents is {average(all_ages)}. ")
+    report.write(f"{male_cnt} males ({percentage_gender}%) and {female_cnt} females {100-percentage_gender}%) have "
+                 f"answered this survey\n")
+    report.write(f"Overall, about {percentage_owners}% own pets, and {100-percentage_owners}% do not.\nThe average age "
+                 f"of those who own pets is {average(owners_ages)}, while the average age of those who do not is "
+                 f"{average(non_owners_ages)}\n")
+
+    if male_owners > female_owners:
+        report.write(f"There are more male pet owners ({percentage_gender_owners}%) than female ones "
+                     f"({100-percentage_gender_owners}%).\n")
+    elif male_owners < female_owners:
+        report.write(f"There are more female owners ({100-percentage_gender_owners}%) than female ones "
+                     f"({percentage_gender_owners}%).\n")
+    else:
+        report.write("There are just as many male and female pet owners.\n")
+
+
 
 
 def average(list):
@@ -45,12 +68,9 @@ def average(list):
     for x in list:
         sum += int(x)
 
-    return sum/len(list)
+    return round(sum/len(list), 2)
 
 def percentage(a, b):
-    return a * 100 / (a + b)
-
-
+    return round(a * 100 / (a + b), 2)
 
 main()
-
