33062084eacfe94fa8b63089b986b84dbcc2d014
Mara Andronache <mararux05@gmail.com>
2023-07-27 10:50:57 +0300

    part of enum implementation

--- survey_oop/survey_oop.py
+++ survey_oop/survey_oop.py
@@ -1,3 +1,6 @@
+from enum import Enum
+
+
 class Question:
     def __init__(self, prompt, validation=None):
         self.prompt = prompt
@@ -10,40 +13,27 @@ class Question:
                 return answer
 
 
+class QuestionEnum(Enum):
+    Q1 = "What is your age? "
+    Q2 = "What city are you from?"
+    Q3 = "What is your gender?"
+    Q4 = "What type of education did you finish last?(middle school, high school, bachelor degree, master degree, PhD) "
+    Q5 = "Do you own pets? "
+    GENDER = ("male", "female")
+    EDUCATION = ("middle school", "high school", "college", "bachelor degree", "master degree", "phd")
+
+
 class UserInfo:
     def __init__(self):
-        self.age = self.get_age()
-        self.city = self.get_city()
-        self.gender = self.get_gender()
-        self.education = self.get_education()
-        self.has_pets = self.has_pets()
-
-    def get_age(self):
-        age = Question("What is your age? ", validation=lambda x: x.isnumeric())
-        return int(age.ask())
-
-    def get_city(self):
-        city = Question("What city are you from? ", validation=lambda x: x.isalpha())
-        return city.ask()
-
-    def get_gender(self):
-        gender_tuple = ("male", "female")
-        gender = Question("What is your gender? ", validation=lambda x: x.lower() in gender_tuple)
-        return gender.ask()
-
-    def get_education(self):
-        education_tuple = ("middle school", "high school", "college", "bachelor degree", "master degree", "phd")
-        education = Question("What type of education did you finish last? (middle school, high school, bachelor degree,"
-                             " master degree, PhD) ", validation=lambda x: x.lower() in education_tuple)
-        return education.ask()
-
-    def has_pets(self):
-        has_pets = Question("Do you have pets? ", validation=lambda x: x.lower() == "yes" or x.lower() == "no")
-        answer = has_pets.ask()
-        if answer == 'no':
-            return False
-
-        return True
+        self.age = Question(QuestionEnum.Q1.value, validation=lambda x: x.isnumeric()).ask()
+        self.city = Question(QuestionEnum.Q2.value, validation=lambda x: x.isalpha()).ask()
+        self.gender = Question(QuestionEnum.Q3.value, validation=lambda x: x.lower() in QuestionEnum.GENDER.value).ask()
+        self.education = Question(QuestionEnum.Q4.value, validation=lambda x: x.lower() in
+                                                                              QuestionEnum.EDUCATION.value).ask()
+        if Question(QuestionEnum.Q5.value, validation=lambda x: x.lower() == "yes" or x == "no").ask() == "yes":
+            self.has_pets = True
+        else:
+            self.has_pets = False
 
     def summary_report(self):
         if self.has_pets:
