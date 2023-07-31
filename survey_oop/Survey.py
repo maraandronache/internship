3d00791fb72adef912f5cf27af9b49d2b377d5c5
Mara Andronache <mararux05@gmail.com>
2023-07-31 10:34:33 +0300

    first task done

--- survey_oop/Survey.py
+++ survey_oop/Survey.py
@@ -4,19 +4,18 @@ from QuestionsEnum import QuestionEnum
 class Survey:
     def __init__(self):
         self.pet_dict = {}
-        self.dict = {}
+        self.answers = {}
 
 
     def start(self):
-        self.age = QuestionEnum.ASK_AGE.value.ask()
-        self.city = QuestionEnum.ASK_CITY.value.ask()
-        self.gender = QuestionEnum.ASK_GENDER.value.ask()
-        self.education = QuestionEnum.ASK_EDUCATION.value.ask()
+        for x in (QuestionEnum.ASK_AGE, QuestionEnum.ASK_CITY, QuestionEnum.ASK_GENDER, QuestionEnum.ASK_EDUCATION):
+            self.answers[x] = x.value.ask()
+
         if QuestionEnum.ASK_HAS_PETS.value.ask().lower() == "yes":
-            self.has_pets = True
+            self.answers[QuestionEnum.ASK_HAS_PETS] = True
         else:
-            self.has_pets = False
-        if self.has_pets:
+            self.answers[QuestionEnum.ASK_HAS_PETS] = False
+        if self.answers[QuestionEnum.ASK_HAS_PETS]:
             self.no_of_pets = QuestionEnum.ASK_NO_OF_PETS.value.ask()
             self.store_pet_info()
         else:
@@ -33,22 +32,11 @@ class Survey:
             type = QuestionEnum.ASK_TYPE_OF_PETS.value.ask()
             self.pet_dict[name] = type.lower()
 
-    def add_answer(self, question, answer):
-        self.dict[question] = answer
-
-    def add_answers(self):
-        self.add_answer(QuestionEnum.ASK_AGE, self.age)
-        self.add_answer(QuestionEnum.ASK_CITY, self.city)
-        self.add_answer(QuestionEnum.ASK_GENDER, self.gender)
-        self.add_answer(QuestionEnum.ASK_EDUCATION, self.education)
-        self.add_answer(QuestionEnum.ASK_HAS_PETS, self.has_pets)
-        return self.dict
-
     def write(self):
         with open("collected_data.csv", 'a') as file:
-            for x in self.dict.keys():
-                file.write(f"{self.dict[x]},")
-            if self.has_pets:
+            for x in self.answers.keys():
+                file.write(f"{self.answers[x]},")
+            if self.answers[QuestionEnum.ASK_HAS_PETS]:
                 file.write(f"{self.no_of_pets}")
                 for x in self.pet_dict.keys():
                     file.write(f",{x},{self.pet_dict[x]}")
--- survey_oop/main.py
+++ survey_oop/main.py
@@ -3,6 +3,5 @@ from Survey import Survey
 if __name__ == "__main__":
     user_input = Survey()
     user_input.start()
-    user_input.add_answers()
     user_input.write()
 
