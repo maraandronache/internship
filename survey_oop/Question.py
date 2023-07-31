7f1325d6b971067d8cafd8ae289551b38c5a2c3a
Mara Andronache <mararux05@gmail.com>
2023-07-31 11:55:40 +0300

    errors

--- survey_oop/Question.py
+++ survey_oop/Question.py
@@ -3,9 +3,13 @@ class Question:
     def __init__(self, prompt, validation=None):
         self.prompt = prompt
         self.validation = validation
+        self.wrong_answers = 0
 
     def ask(self):
-        while True:
+        while self.wrong_answers < 3:
             answer = input(self.prompt)
             if not self.validation or self.validation(answer):
                 return answer
+            self.wrong_answers += 1
+        else:
+            raise SyntaxError(f"You have answered the question '{self.prompt}' with an invalid input 3 times. Please restart the survey.")
